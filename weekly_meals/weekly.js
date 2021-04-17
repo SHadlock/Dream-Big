955f7154f10a6441ef5abc37e717fb8ed9d67b9b
SHadlock <80804252+SHadlock@users.noreply.github.com>
2021-04-17 10:04:22 -0600

    changes to website

--- weekly_meals/weekly.js
+++ weekly_meals/weekly.js
@@ -1,28 +1,99 @@
-function dragStart(event) {
-    event.dataTransfer.setData("Text", event.target.id); //dataTransfer holds data that is going to be dragged. setData sets the format of data. 
+
+
+function myFunction() {
+    $.ajax({
+        url: "http://localhost:8080/recipes?user_id=1", success: function (data) {
+            for (var i in data) {
+                var meals = document.getElementById('meals');
+                var div = document.createElement('div');
+                var para = document.createElement('p');
+                para.id = 'drop_' + data[i].id;
+                div.id = 'dropTarget';
+                para.innerHTML = data[i].name
+                para.draggable = "true";
+                div.appendChild(para);
+                meals.appendChild(div);
+                div.addEventListener("drop", drop);
+                div.addEventListener("dragover", allowDrop);
+                para.addEventListener("dragstart", dragStart);
+            };
+
+
+
+        }
+    });
+    var weekElement = document.getElementById("week");
+
+    //define a date object variable that will take the current system date
+    todaydate = new Date();
+
+    //find the year of the current date
+    var oneJan = new Date(todaydate.getFullYear(), 0, 1);
+
+    // calculating number of days in given year before a given date 
+    var numberOfDays = Math.floor((todaydate - oneJan) / (24 * 60 * 60 * 1000));
+    // weekElement.value = weekElement.defaultValue;
+
+    // adding 1 since to current date and returns value starting from 0 
+    var result = Math.ceil((todaydate.getDay() + 1 + numberOfDays) / 7);
+
+    var d = new Date();
+    var n = d.getFullYear();
+
+
+    var weekNumber = n + "-" + "W" + result;
+    weekElement.value = weekNumber;
+
+}
+// Drag and drop meals
+function dragStart(ev) {
+    console.log("dragStart")
+    ev.dataTransfer.setData("Text", ev.target.id); //dataTransfer holds data that is going to be dragged. setData sets the format of data. 
+    ev.effectAllowed = "copy";
     //event.dataTransfer.setData(format, data)
     //event.target.id returns the element (id) that targeted the event.
-    console.log(dragStart);
-
-}
-
-function dragging(event) {
-    document.getElementById("demo").innerHTML = "The p element is being dragged";
-    //changes text when dragging element
 }
 
 
-function allowDrop(event) {
-    event.preventDefault(); //will only allow to go to dropzone
+
+
+function allowDrop(ev) {
+    ev.preventDefault(); //will only allow to go to dropzone
 }
 
-function drop(event) {
-    event.preventDefault(); //if outside of the area, won't drop
-    var data = event.dataTransfer.getData("Text"); //variable of "data" is set to retrieve the data that is to be dragged
-    event.target.appendChild(document.getElementById(data)); //puts variable of "data" in new box
-    // document.getElementById("demo").innerHTML = "The p element was dropped"; //changes text when being dropped
+function drop(ev) {
+    ev.preventDefault(); //if outside of the area, won't drop
+    //var data = ev.dataTransfer.getData("Text"); //variable of "data" is set to retrieve the data that is to be dragged
+    var id = ev.dataTransfer.getData("Text");
+    var nodeCopy = document.getElementById(id).cloneNode(true);
+    nodeCopy.id = "newId"
+    ev.target.appendChild(nodeCopy);
 }
 
+
+
+// Calendar
+
+//define a date object variable that will take the current system date
+todaydate = new Date();
+
+//find the year of the current date
+var oneJan = new Date(todaydate.getFullYear(), 0, 1);
+
+// calculating number of days in given year before a given date 
+var numberOfDays = Math.floor((todaydate - oneJan) / (24 * 60 * 60 * 1000));
+
+// adding 1 since to current date and returns value starting from 0 
+var result = Math.ceil((todaydate.getDay() + 1 + numberOfDays) / 7);
+
+// var d = new Date();
+// var n = d.getFullYear();
+
+
+// var weekNumber = "2021-W01";
+
+
+// console.log(weekNumber)
 // define variables
 var nativePicker = document.querySelector('.nativeWeekPicker');
 var fallbackPicker = document.querySelector('.fallbackWeekPicker');
